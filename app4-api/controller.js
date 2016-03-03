aa4ed7c50e3ec71925abf78f7cd31feb460a8f40
alexdecapri <alexdecapri@gmail.com>
2016-03-03 13:44:28 -0700

    don't have access to images, but it should work if I did

--- app4-api/controller.js
+++ app4-api/controller.js
@@ -1,4 +1,14 @@
 var app = angular.module("apiApp");
 app.controller("controller", function($scope, service) {
 
+  $scope.smurfGetter = function() {
+    service.getSmurfs()
+    .then(function(response) {
+      console.log("response from controller:", response);
+      $scope.smurfArr = response;
+      // console.log($scope.smurfArr);
+    })
+  };
+  $scope.smurfGetter();
+
 });
--- app4-api/service.js
+++ app4-api/service.js
@@ -1,4 +1,17 @@
 var app = angular.module("apiApp");
- app.service("service", function() {
+ app.service("service", function($http) {
+
+   this.getSmurfs = function() {
+     return $http({
+       method: "GET",
+       url: "http://smurfs.devmounta.in/smurfs/"
+     })
+     .then(function(response) {
+       return response.data;
+     })
+     .catch(function(err) {
+       console.error("The error is", err);
+     })
+   }
 
  });
