0f8fbae2d8662af752491c021add2ae57f9ca043
alexdecapri <alexdecapri@gmail.com>
2016-03-03 12:51:48 -0700

    first commit

---
+++ app4-api/controller.js
@@ -0,0 +1,4 @@
+var app = angular.module("apiApp");
+app.controller("controller", function($scope, service) {
+
+});
---
+++ app4-api/service.js
@@ -0,0 +1,4 @@
+var app = angular.module("apiApp");
+ app.service("service", function() {
+
+ });
